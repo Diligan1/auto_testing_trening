b2469d2bd1f528ae3adf8618a1d58d5da3c525aa
iluxa_dilligan <i.ilyin@etpgpb.ru>
2024-05-12 14:10:07 +0300

    добавил файл спортлайн

---
+++ sport_line.py
@@ -0,0 +1,13 @@
+my_list = [1, 2, 3, 4, 5, 6, 1, 1, 1]
+my_list.append(9)
+print(my_list)
+
+my_list.insert(2, 6)
+print(my_list)
+print(len(my_list))
+
+result = my_list.count(1)
+print(result)
+
+my_dict = ['Hello world','2']
+print(len(my_dict))
